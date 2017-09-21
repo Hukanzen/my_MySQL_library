7214d6ad7707cadfafd896c2b06f62e60b9b3eae
Hukanzen <qazxswed1934@gmail.com>
2017-09-22 05:56:32 +0900

    new

--- PHP/class_sample.php
+++ PHP/class_sample.php
@@ -2,7 +2,7 @@
 require_once("./class_mysqli_connection.php");
 
 
-$mysqli=mysqli_connection->db_connect("localhost","test_user","password","test_db");
+$mysqli=new mysqli_connection("localhost","test_user","password","test_db");
 var_dump($mysqli->db_fetch("select * from test_db.test_table;"));
 
 ?>
