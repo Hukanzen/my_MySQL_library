2bd839eaaf81add210404a9e0cdedd68eb52cad3
Hukanzen <qazxswed1934@gmail.com>
2017-09-22 05:51:48 +0900

    関数ミス

--- PHP/sample.php
+++ PHP/sample.php
@@ -1,7 +1,7 @@
 <?php
 require_once("./mysqli_connection.php");
 
-$linkid=db_connection("localhost","test_user","password","test_db");
+$linkid=db_connect("localhost","test_user","password","test_db");
 var_dumper(db_fetch("select * from test_db.test_table",$linkid));
 db_close($linkid);
 
