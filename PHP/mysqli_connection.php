4e9be2da643cabaeb7a4bc2d1a98a7b0a825b398
Hukanzen <qazxswed1934@gmail.com>
2017-09-22 04:13:38 +0900

    関数説明

--- PHP/mysqli_connection.php
+++ PHP/mysqli_connection.php
@@ -1,10 +1,12 @@
 <?php
 
-	//mysqli 関連の自作関数
-
+	/* mysqli 関連の自作関数 */
+	
+	/* DBと接続 */
 	function db_connect($server,$user,$pass,$db_name){
 		$linkid=mysqli_connect($server,$user,$pass,$db_name);
 		if(!$linkid) die("Failure mysqli_connect".mysqli_error($linkid));
+		/* 文字コードの指定 */
 		mysqli_set_charset($linkid,"utf8");
 		return $linkid;
 	}
@@ -14,18 +16,21 @@
 //		if(!$db_rslt) die("Failure select db".mysqli_error($linkid));
 //	}
 
+	/* クエリの作成 */
 	function db_query($SQL,$linkid){
 		$rslt=mysqli_query($linkid,$SQL);
 		if(!$rslt) die("$SQL is Failure".mysqli_error($linkid));
 		return $rslt;
 	}
 
+	/* SQL文で取得できるデータの個数を返す．*/
 	function db_num_rows($SQL,$linkid){
 		$rslt=db_query($SQL,$linkid);
 		$num=mysqli_num_rows($rslt);
 		return $num;
 	}
 
+	/* SQL文を投げて，データを取得し，配列で返す */
 	function db_fetch($SQL,$linkid){
 		$data=array();
 		$rslt=db_query($SQL,$linkid);
@@ -35,6 +40,7 @@
 		return $data;
 	}
 
+	/* 配列のSQL文を投げて，クエリを取得し，配列で返す */
 	function db_a_query($aSQL,$linkid){
 		$aRSLT=array();
 		foreach($aSQL as $sql){
@@ -48,6 +54,7 @@
 		return $aRSLT;
 	}
 
+	/* 配列のSQL文を投げて，データを取得し，配列で返す*/
 	function db_a_fetch_assoc($aSQL,$linkid){
 		$aRSLT=db_a_query($aSQL,$linkid);
 		$aFET=array();
@@ -62,6 +69,7 @@
 		return $aFET;
 	}
 
+	/* DBとの接続を切断する */
 	function db_close($linkid){
 		mysqli_close($linkid);
 	}
