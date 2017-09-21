367575c70d5eb6ab20a880da6927cf2793ee8804
Hukanzen <qazxswed1934@gmail.com>
2017-09-22 06:01:39 +0900

    括弧

--- PHP/class_sample.php
+++ PHP/class_sample.php
@@ -4,6 +4,6 @@ require_once("./class_mysqli_connection.php");
 
 $mysqli=new mysqli_connection("localhost","test_user","password","test_db");
 
-var_dump($mysqli->db_a_fetch_assoc(array("select * from test_db.test_table;"));
+var_dump($mysqli->db_a_fetch_assoc(array("select * from test_db.test_table;")));
 
 ?>
